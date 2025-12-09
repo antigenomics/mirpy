b0b5bee182417b9b96d1abfd512ee08518517097
MichaelPodsytnik <flopper59@mail.ru>
2025-12-09 16:57:39 +0300

    Add generate seq with meta

--- mir/basic/pgen.py
+++ mir/basic/pgen.py
@@ -1,8 +1,10 @@
 from multiprocessing import Pool
+import math
 
 import olga.load_model as load_model
 import olga.generation_probability as pgen
 import olga.sequence_generation as seq_gen
+from olga.utils import nt2aa
 
 from mir import get_resource_path
 
@@ -18,6 +20,8 @@ class OlgaModel:
 
         :param model: a path to the directory where the OLGA model is stored
         """
+        self.is_d_present = is_d_present
+
         # Define the files for loading in generative model/data
         params_file_name = f'{model}/model_params.txt'
         marginals_file_name = f'{model}/model_marginals.txt'
@@ -32,13 +36,15 @@ class OlgaModel:
             generative_model = load_model.GenerativeModelVJ()
         genomic_data.load_igor_genomic_data(params_file_name, V_anchor_pos_file, J_anchor_pos_file)
         generative_model.load_and_process_igor_model(marginals_file_name)
+        self.v_names = [x[0] for x in genomic_data.__dict__['genV']]
+        self.j_names = [x[0] for x in genomic_data.__dict__['genJ']]
         if is_d_present:
             self.pgen_model = pgen.GenerationProbabilityVDJ(generative_model, genomic_data)
             self.seq_gen_model = seq_gen.SequenceGenerationVDJ(generative_model, genomic_data)
         else:
             self.pgen_model = pgen.GenerationProbabilityVJ(generative_model, genomic_data)
             self.seq_gen_model = seq_gen.SequenceGenerationVJ(generative_model, genomic_data)
-        
+
     def compute_pgen_cdr3nt(self, cdr3nt: str):
         """
         A function to compute the TCR generation probability for the nucleotide sequence
@@ -83,6 +89,168 @@ class OlgaModel:
         res = []
         for i in range(n):
             res.append(self.seq_gen_model.gen_rnd_prod_CDR3()[1])
+        return res
+
+    def _gen_one_vdj_with_meta(self, conserved_J_residues: str = "FVW") -> dict:
+        """
+        Generate one productive VDJ CDR3 with meta:
+        cdr3 (aa), cdr3_nt, v_gene, j_gene, v_end, j_start
+        """
+        sg = self.seq_gen_model
+
+        while True:
+            recomb_events = sg.choose_random_recomb_events()
+
+            V_seq_full = sg.cutV_genomic_CDR3_segs[recomb_events["V"]]
+            if len(V_seq_full) <= max(recomb_events["delV"], 0):
+                continue
+
+            D_seq_full = sg.cutD_genomic_CDR3_segs[recomb_events["D"]]
+            J_seq_full = sg.cutJ_genomic_CDR3_segs[recomb_events["J"]]
+
+            if len(D_seq_full) < (
+                recomb_events["delDl"] + recomb_events["delDr"]
+            ):
+                continue
+            if len(J_seq_full) < recomb_events["delJ"]:
+                continue
+
+            V_seq = V_seq_full[: len(V_seq_full) - recomb_events["delV"]]
+            D_seq = D_seq_full[
+                recomb_events["delDl"] : len(D_seq_full) - recomb_events["delDr"]
+            ]
+            J_seq = J_seq_full[recomb_events["delJ"] :]
+
+            if (
+                len(V_seq)
+                + len(D_seq)
+                + len(J_seq)
+                + recomb_events["insVD"]
+                + recomb_events["insDJ"]
+            ) % 3 != 0:
+                continue
+
+            insVD_seq = seq_gen.rnd_ins_seq(
+                recomb_events["insVD"], sg.C_Rvd, sg.C_first_nt_bias_insVD
+            )
+            insDJ_seq = seq_gen.rnd_ins_seq(
+                recomb_events["insDJ"], sg.C_Rdj, sg.C_first_nt_bias_insDJ
+            )[::-1]
+
+            ntseq = V_seq + insVD_seq + D_seq + insDJ_seq + J_seq
+            aaseq = nt2aa(ntseq)
+
+            if "*" in aaseq:
+                continue
+            if aaseq[0] != "C" or aaseq[-1] not in conserved_J_residues:
+                continue
+
+            L_V_nt = len(V_seq)
+            L_J_nt = len(J_seq)
+            L_aa = len(aaseq)
+
+            V_len_aa = L_V_nt // 3
+            J_len_aa = L_J_nt // 3
+
+            v_end = V_len_aa
+            j_start = L_aa - J_len_aa + 1
+
+            v_name = self.v_names[recomb_events["V"]]
+            j_name = self.j_names[recomb_events["J"]]
+
+            return {
+                "cdr3": aaseq,
+                "cdr3_nt": ntseq,
+                "v_gene": v_name,
+                "j_gene": j_name,
+                "v_end": v_end,
+                "j_start": j_start,
+            }
+
+    def _gen_one_vj_with_meta(self, conserved_J_residues: str = "FVW") -> dict:
+        """
+        Generate one productive VJ CDR3 with meta:
+        cdr3 (aa), cdr3_nt, v_gene, j_gene, v_end, j_start
+        """
+        sg = self.seq_gen_model
+
+        while True:
+            recomb_events = sg.choose_random_recomb_events()
+
+            V_seq_full = sg.cutV_genomic_CDR3_segs[recomb_events["V"]]
+            if len(V_seq_full) <= max(recomb_events["delV"], 0):
+                continue
+
+            J_seq_full = sg.cutJ_genomic_CDR3_segs[recomb_events["J"]]
+            if len(J_seq_full) < recomb_events["delJ"]:
+                continue
+
+            V_seq = V_seq_full[: len(V_seq_full) - recomb_events["delV"]]
+            J_seq = J_seq_full[recomb_events["delJ"] :]
+
+            if (
+                len(V_seq)
+                + len(J_seq)
+                + recomb_events["insVJ"]
+            ) % 3 != 0:
+                continue
+
+            insVJ_seq = seq_gen.rnd_ins_seq(
+                recomb_events["insVJ"], sg.C_Rvj, sg.C_first_nt_bias_insVJ
+            )
+
+            ntseq = V_seq + insVJ_seq + J_seq
+            aaseq = nt2aa(ntseq)
+
+            if "*" in aaseq:
+                continue
+            if aaseq[0] != "C" or aaseq[-1] not in conserved_J_residues:
+                continue
+
+            L_V_nt = len(V_seq)
+            L_J_nt = len(J_seq)
+            L_aa = len(aaseq)
+
+            V_len_aa = L_V_nt // 3
+            J_len_aa = L_J_nt // 3
+
+            v_end = V_len_aa
+            j_start = L_aa - J_len_aa + 1
+
+            v_name = self.v_names[recomb_events["V"]]
+            j_name = self.j_names[recomb_events["J"]]
+
+            return {
+                "cdr3": aaseq,
+                "cdr3_nt": ntseq,
+                "v_gene": v_name,
+                "j_gene": j_name,
+                "v_end": v_end,
+                "j_start": j_start,
+            }
+
+    def generate_sequences_with_meta(self, n: int = 1000) -> list[dict]:
+        """
+        Generate n sequences with meta:
+          - cdr3 (aa)
+          - cdr3_nt
+          - v_gene, j_gene
+          - v_end, j_start (AA, 1-based)
+          - pgen_raw, pgen (log10)
+        """
+        res = []
+        for _ in range(n):
+            if self.is_d_present:
+                rec = self._gen_one_vdj_with_meta()
+            else:
+                rec = self._gen_one_vj_with_meta()
+
+            p_raw = self.compute_pgen_cdr3aa(rec["cdr3"])
+            rec["pgen_raw"] = p_raw
+            rec["pgen"] = (
+                math.log10(p_raw) if (p_raw is not None and p_raw > 0) else float("-inf")
+            )
+            res.append(rec)
         return res
 
     # TODO: v usage correction
